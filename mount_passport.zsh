18255399f64cb5781908195ff9051a11ff7a7fbc
HarryLong <hjlong1@gmail.com>
2014-07-21 14:57:55 +0200

    modifying scripts to reflect new external hard drive partition setup

---
+++ mount_passport.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/zsh
+
+sudo mount /mnt/passport/ext2
+sudo mount /mnt/passport/exfat
+
--- open_secure_partition.zsh
+++ open_secure_partition.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
-sudo cryptsetup luksOpen /media/harry/3612e1c7-7940-47ca-a2fc-77f2c9aa92a8/partitions/partition1 LUKS
+sudo cryptsetup luksOpen /mnt/passport/ext2/partitions/partition1 LUKS
 sudo mount /dev/mapper/LUKS /mnt/LUKS
 
